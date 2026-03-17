2e186bfbe8bfdab79a9767cea7dee0c7e029aacd
MarinaKhristosova <108974887+MarinaKhristosova@users.noreply.github.com>
2026-03-17 13:08:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // --- ТВОИ НАСТРОЙКИ ---
 const APP_CONFIG = {
     defaults: {
-        exchangeRate: 3.5898,   // Курс доллара
+        exchangeRate: 3.7287,   // Курс доллара
         multisport: 154.65,    // Цена мультиспорта
-        lunch: 1000.00         // Обеды
+        lunch: 800.00         // Обеды
     },
     labels: {
         base: "Base Amount (USD)",
